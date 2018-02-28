c5cb4a70d147e08126032590491cf70c000acb1c
Weng Chiyu <cyrain@gmail.com>
2018-02-28 21:32:52 +0800

    update for data.table dev 1.10.5

--- benchmark_mcapply01.R
+++ benchmark_mcapply01.R
@@ -1,6 +1,12 @@
 # benchmark purrr, mcapply, data.table and future.lapply
 # modified from https://scottishsnow.wordpress.com/2018/02/18/dplyr-lapply-for-loop/
 # by Mike Spencer, also see his tweet https://twitter.com/MikeRSpencer/status/966435928444153858
+
+# update for data.table dev 1.10.5 (News.md https://github.com/Rdatatable/data.table/blob/master/NEWS.md )
+# install.packages("data.table", type = "source", repos = "http://Rdatatable.github.io/data.table")
+
+# Not a critical example, because each file is small.
+
 library(tidyverse)
 library(parallel)
 library(microbenchmark)
@@ -87,25 +93,37 @@ all.equal(tt1,tt2)
 tt3 <-purrr_foo(f)
 all.equal(tt1,tt3 %>% data.table())
 
+datatable_foo <- function(f) {
+  rbindlist(lapply(
+    paste0("./temp/raw/", f),
+    fread, select=c("date", "a", "b", "c")
+  )) %>% melt.data.table(id.vars = "date")
+}
+
+all.equal(tt1 %>% setorder(date) ,datatable_foo(f) %>% setorder(date))
+
 # Benchmark
 mtimes <- microbenchmark ( ## will crash
   #'forloop'= loop_foo(f),
   'purrr'= purrr_foo(f),
+  'datatable'= datatable_foo(f),
   'lapply_future' = rbindlist(future_lapply(f, fread_clean_fwrite)),
   'mclapply' = rbindlist(mclapply(f, mc.cores=4, read_clean_write)),
   'mclapply_data.table'= rbindlist(mclapply(f, mc.cores=4, fread_clean_fwrite)),
   times = 50L
 )
-#Unit: milliseconds
-#expr       min        lq      mean    median       uq      max neval
-#purrr 726.75689 765.00129 803.02083 786.82449 832.1882 907.0319    50
-#lapply_future 115.70859 126.45762 157.86677 141.08902 166.0428 457.2523    50
-#mclapply 211.09944 224.22713 275.19899 242.54487 309.0524 479.8111    50
-#mclapply_data.table  58.77066  68.60537  94.84471  77.21572  91.4194 369.0396    50
+#Unit: milliseconds data.table version 1.10.5 use 32G RAM)
+#expr       min        lq      mean    median        uq      max neval
+#purrr 760.60659 790.45448 824.13510 805.96776 836.95366 965.1273    50
+#datatable  99.35356 106.83779 116.64021 111.58510 121.26902 243.1460    50
+#lapply_future 124.23986 141.22057 162.75011 152.70512 165.78870 309.2831    50
+#mclapply 222.91257 233.16105 269.45015 258.72337 293.84481 522.2993    50
+#mclapply_data.table  65.34642  78.46321  89.96808  82.80777  98.55239 226.5945    50
 
 times = list(
   #forloop = system.time(loop_foo(f)),
   purrr= system.time(purrr_foo(f)),
+  datatable= system.time(datatable_foo(f)),
   lapply_future = system.time(rbindlist(future_lapply(f, fread_clean_fwrite))),
   mclapply = system.time(rbindlist(mclapply(f, mc.cores=4, read_clean_write))),
   mclapply_data.table= system.time(rbindlist(mclapply(f, mc.cores=4, fread_clean_fwrite)))
@@ -116,7 +134,7 @@ names(x) = substr(names(x), 1, nchar(names(x)) - 8)
 
 par(mar=c(5, 8.5, 4, 2) + 0.1)
 barplot(x, names.arg=names(x),
-        main="Elapsed time on an Intel i5 4460 with 4 cores at 3.2GHz",
+        main="Elapsed time on an Intel i7 7700 with 4 cores at 3.6GHz",
         xlab="Seconds", horiz=T, las=1)
-#par(mar=c(5, 4, 4, 2) + 0.1)
+
 
