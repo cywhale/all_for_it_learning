e35c0107ecf33fdde95668f576cb45ff91a4cd52
Weng Chiyu <cyrain@gmail.com>
2020-04-28 20:45:23 +0800

    git a local folder to a repo subdirectory

--- git_clone_submodule_command01.sh
+++ git_clone_submodule_command01.sh
@@ -7,4 +7,10 @@ git commit -m "ThreeGeoJSON submodule"
 git submodule update --init --recursive
 git push origin master:master --progress
 # git fetch --progress --prune origin
- 
+
+# Similar to git a local folder to a repo subdirectory after git clone 
+# But must put your local folder (copy/move) under the directory where you git clone
+# cd git_clone_dir/your_local_folder
+git add . # after that can use git add -A
+git commit -m "git a local folder to a repo subdir"
+git push origin master
