00c64853f0d8d4fbd3c74ab9305b04bfed5fb1bd
bioer <bioer@oc.ntu.edu.tw>
2020-05-02 20:02:47 +0800

    Mongoosegit add -A

--- node_EthanRBrown_book/meadowlark.js
+++ node_EthanRBrown_book/meadowlark.js
@@ -1,28 +1,104 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const multiparty = require('multiparty')
+const cookieParser = require('cookie-parser')
+const expressSession = require('express-session')
+const MongoStore = require('connect-mongo')(expressSession)
+//const RedisStore = require('connect-redis')(expressSession)
 
 const handlers = require('./lib/handlers')
+const weatherMiddlware = require('./lib/weather')
+
+const credentials = require('./credentials')
+
+//require('./db')
+const db = require('./db')
+const db2 = db.mongooseConn //.useDB('test')
 
 const app = express()
 
 // configure Handlebars view engine
 app.engine('handlebars', expressHandlebars({
   defaultLayout: 'main',
+  helpers: {
+    section: function(name, options) {
+      if(!this._sections) this._sections = {}
+      this._sections[name] = options.fn(this)
+      return null
+    },
+  },
 }))
 app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.use(cookieParser(credentials.cookieSec)) //credentials.cookieSecret
+app.use(expressSession({
+  //https://stackoverflow.com/questions/24477035/express-4-0-express-session-with-odd-warning-message
+  cookieName: 'session',
+  secret: credentials.cookieSec,//,
+  duration: 30 * 60 * 1000,
+  activeDuration: 5 * 60 * 1000,
+  httpOnly: true,
+  secure: true,
+  ephemeral: true,
+  resave: true,
+  saveUninitialized: true,
+  store: new MongoStore({
+     mongooseConnection: db2,
+     autoReconnect:true,
+  })
+}))
 
 const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/pub'))
+app.use(weatherMiddlware)
 
 app.get('/', handlers.home)
 
-app.get('/about', handlers.about)
+// handlers for browser-based form submission
+app.get('/newsletter-signup', handlers.newsletterSignup)
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
 
-// custom 404 page
+// handlers for fetch/JSON form submission
+app.get('/newsletter', handlers.newsletter)
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
+
+// vacation photo contest
+// vacation photo contest
+app.get('/contest/vacation-photo', handlers.vacationPhotoContest)
+app.get('/contest/vacation-photo-ajax', handlers.vacationPhotoContestAjax)
+app.post('/contest/vacation-photo/:year/:month', (req, res) => {
+  const form = new multiparty.Form()
+  form.parse(req, (err, fields, files) => {
+    if(err) return handlers.vacationPhotoContestProcessError(req, res, err.message)
+    console.log('got fields: ', fields)
+    console.log('and files: ', files)
+    handlers.vacationPhotoContestProcess(req, res, fields, files)
+  })
+})
+app.get('/contest/vacation-photo-thank-you', handlers.vacationPhotoContestProcessThankYou)
+app.post('/api/vacation-photo-contest/:year/:month', (req, res) => {
+  const form = new multiparty.Form()
+  form.parse(req, (err, fields, files) => {
+    if(err) return handlers.api.vacationPhotoContestError(req, res, err.message)
+    handlers.api.vacationPhotoContest(req, res, fields, files)
+  })
+})
+
+// vacations
+app.get('/vacations', handlers.listVacations)
+app.get('/notify-me-when-in-season', handlers.notifyWhenInSeasonForm)
+app.post('/notify-me-when-in-season', handlers.notifyWhenInSeasonProcess)
+
+// utility routes
+app.get('/set-currency/:currency', handlers.setCurrency)
+
 app.use(handlers.notFound)
-
-// custom 500 page
 app.use(handlers.serverError)
 
 if(require.main === module) {
