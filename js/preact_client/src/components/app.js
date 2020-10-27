e3a3ba820c20ffd98610197c7f66f83e0ea6434f
cywhale <cyrain@gmail.com>
2020-10-27 20:16:47 +0800

    small fix a temp version try redirect

--- js/preact_client/src/components/app.js
+++ js/preact_client/src/components/app.js
@@ -1,20 +1,24 @@
 import { h } from 'preact';
 import { Router } from 'preact-router';
+import { createMemoryHistory } from 'history'; //createHashHistory
 import Header from './header';
+import Redirect from './Redirect';
 
 // Code-splitting is automated for `routes` directory
 import Home from '../routes/home';
 import Profile from '../routes/profile';
 
-const App = () => (
-  <div id="app">
-    <Header />
-    <Router>
+const App = () => {
+  return(
+    <div id="app">
+      <Header />
+      <Router history={createMemoryHistory()}>
 	<Home path="/" />
         <Profile path="/profile/" user="me" />
 	<Profile path="/profile/:user" />
-    </Router>
-  </div>
-)
-
+        <Redirect path="/login" to="http://localhost:3000" />
+      </Router>
+    </div>
+  );
+};
 export default App;
