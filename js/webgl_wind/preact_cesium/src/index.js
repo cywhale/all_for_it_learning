fdd210693dc18e5ee0cab0b43a151e744aa84117
cywhale <cyrain@gmail.com>
2020-11-17 09:53:17 +0800

    remove not-work-yet preactjs module

--- js/webgl_wind/preact_cesium/src/index.js
+++
@@ -1,8 +0,0 @@
-import './style';
-import App from './components/app';
-import "cesium/Source/Widgets/widgets.css";
-
-import buildModuleUrl from "cesium/Source/Core/buildModuleUrl";
-buildModuleUrl.setBaseUrl('./');
-
-export default App;
