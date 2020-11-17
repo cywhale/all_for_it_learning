b64a64d4e2e41482363ae7caf6c97333902e3663
cywhale <cyrain@gmail.com>
2020-11-17 14:30:37 +0800

    small fix path demo/ to serve http://[::]:1337/demo

--- js/webgl_wind/demo/index.js
+++ js/webgl_wind/demo/index.js
@@ -82,10 +82,10 @@ getJSON('https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_110m_coastl
 });
 
 function updateWind(name) {
-    getJSON('wind/' + windFiles[name] + '.json', function (windData) {
+    getJSON('demo/wind/' + windFiles[name] + '.json', function (windData) {
         const windImage = new Image();
         windData.image = windImage;
-        windImage.src = 'wind/' + windFiles[name] + '.png';
+        windImage.src = 'demo/wind/' + windFiles[name] + '.png';
         windImage.onload = function () {
             wind.setWind(windData);
         };
