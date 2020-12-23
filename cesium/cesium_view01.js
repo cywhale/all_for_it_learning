45231b1127b3f6aaf70613fb25f6858e8ddfe532
cywhale <cyrain@gmail.com>
2020-12-23 16:53:42 +0800

    cesiumjs return zoom level not work if not 3D, use pixelSize instead (roughly)

--- cesium/cesium_view01.js
+++ cesium/cesium_view01.js
@@ -1,21 +1,45 @@
 // run in cesium sandcastle: 
-// https://sandcastle.cesium.com/#c=rVfbUuM4EP0VFS9xBiIzl4clyVALDAtUcSvILFU7TFGK3YkFtuSR5ITMFv++Lcl2bBKy8zB5SSz1OepudZ92ZkyRGYc5KPKZCJiTI9C8yOjfbi2434rc85EUhnEB6n6rO7gXM0TNQZsdomVhkh0CzD4IqUxSbhsp0zGzrLGMigyEoVMwxynYn4eLszjolCadijFiGSiGCO8Q9c+4eS8mhYgMl4LEYCAy/0iZncMM0iDm2jARQZf8ey8IflIwREcgYMnjHgfLbcNTuFZyxmMXtdum01SOgT7oQk1YBLRp04D+KFhsFFjypgV9qDYatrFicy6mh8VkAuoU+DQxzdgwZE0Tt9wAaQ1fQMiMC2akep0LOlGFNng9bTOXIsswQUhgaVKbHETvDsqfw8/k/V75sL1dZ6s6NmPPJyAzMIpHx0q5k1sB4t25hF+wZ54VWds4cLTdQZsTSp5glfzduuR0SUjq+0STdoxNdj4hgWcf1jdCM+/ZbYQP4jbHS3RntUK1HwWmUMJnosH54n++VKkszUSRWiu3HIaJMbnuh+GUa4p+Rk/wHCVMTIFGMgt/FNgSWKQ6fP9hb2/3Y+h7p/cx7tmyVRgJhtwzCfQylvd0xFLoyYlbcNcc91wVYjeUl57K6IlKMeL4xeL4eIa9c44ZwoJVQd0TQR2j7Ulb0bYp8Xtvb6+M0PanbXDboPjd8zt+LwzJ0cHX0dnVZZ8cROizRi/JQ674jBkg2JmcjVPQhGskMEROJjziLE0XRBd5jpQQEybimq3+YGDk4PoMG1uQsQL2RJhBywWWVgZkzk2CjpI5UzYv1OOddmDl6ZG8AeE7tNnHmxqVPrSQh4sRPOMtwpEshKnjtdVTqlwMExS1OGjBuq2Ssf6IIjtQii102RW1KU1BTJ3kVeauAy3m0XffI5ZoDR+Q7e3HlYK01lUVr5zw7fH7oG2+xv0a3V0hb4QwKvlr61XvW1HYHuY+Cu6jcAw2CL72nOoshQqNTZFC87Bv/DutNwbrwWX5XjCTUGyWwI+YGkXtc/cNbFXzNbicTEu0W3gLXjaIR7PnwA+0Jdg+v4WtuqoGu4Um2i2sg7+0l17e1iObWB0pZqLkppHfxsiul4PqpOoyVsYIqlVeGLBDfola02WQpjzXksc7S8deO9E8LJd65Sxc41amGmYRU0ZOFcsTHqF96f9xdRi9O7n94xO1VrjDxEgeNQABEjbPTKrJ2mRtD1ZrVk3E1y8Q3rBJOBbxbZ6AsukNw9K5Q5SQGFXK7+Acltm15MLopjsrhbG8nDa+dUM5f4b0lv8EF4PLGU7b62o1qP3ZaQtha4Te8diW3NsGp604w7AsAdvWYxRZOfF+aBRtFGmp+BTnbkp8BQBheQ5M4a4kdkDVFYkOnolrj8RR/wHndu0vVSzmhbZzvY4Rzw/flZIEc5dCK3n0ofQcBxxWC2oTjrug1pjc2vWbmlMT9ls+NOo0kqlU/Sr/R/aJntwcH182bPzl3wDmB/Clo7Y+ba/To/ODi+uH0dXDyc3V18svFUHVo90BeRfa38sB46Rsn+zt7raksnzlpVzgBD8dXZxj8J1zGTE3yO10fRJyLminvKcXvADM/maGdvF17vDoPumQ7Soap0tGfoGpFWLnWhef/8KMxcGnLhp2hmMV7iPkFdWtlcxNXF5Tf43smG32y0nsr1FdWj3dxOUF99fIMvtupkkOZSt63mVXNpDkFbTSthKC6rfB2BebN12jVZugP1GuvIJ5uBezDYBez0mBU/8+6fU6m4z/r2AsCf2dVeMIf2fpOMLfWT+OcKWIBqQeyi9WSbd2tobaLFLYr/j/5Jl9ISaFSgNKQwNZnuJLtA7HBf5ZMDTS9bAYhk3oMOYzwuPPa/5wkyhlWuPOBP+L2Iq839ofhmi/Ak0lszPmagYqZQtrlrzfP/eLlNJhiI/rkaWqvGL+Dw
+// https://sandcastle.cesium.com/#c=rVhbU9s6EP4rGl7iFCxzLZdQ5gClLTO0ZUpaZk7TYRRbTgS25EpyuJzhv5+VZDtyEkIfmuk0kbzfane1++2aCZFowug9legd4vQenVLFyhz/sHvBYCW261PBNWGcysFKtzfgE0DdU6XXkBKlHq8hSsyCC6nH1WMtRDYkRmsi4jKnXOMR1WcZNT9PHs+ToFOJdGqNMcmpJIBwBmG3hocDHkW5SFjKaIJSKXI01rpQB1E0Ygr0Mj0uhzgWeUSfnmiU7OzSvZ1dsrm3+3Z3N94fbm8Nd8hbsr21Cb82tgc8LXmsmeAooZrG+l8h8gs6oVmQgD7CY9pF/w04gk9GNVIx5XRqll32po81y+ilFBOW2CDax3iUiSHFN6qUKYkp9mU86O+SJFpSo9yXwDf1A082keSe8dFJmaZUfqJsNNZ+qCCCCo/ttgdSir6nXOSMEy3kbGhxKkul4bbbYv6hZhdgm3gdvUGfiR5jiE+wjndgOaPkJhWTx64FOwWx4EpkFGdiFLRP6PbmJQYr9rADNFhBq0t0I5SCJ4GxLjN3Btat96qfh+/Qxn61WF1tLrH2JicPH6nIqZYsPpPSBqQVd8hQmwefyQPLy7wtHFi1tRG1k7NOzJ1ROTS3P1U0E4UqBStck5G9ti+0sj+Yd+rNolzpogg16W1ETLBfsoF6htNlxtZ5inMXsasYFvyqgJz3nX9NzHeOpShwzh2+jvNv2Hwk1aXkLgE8nc/u57OhEk+Ml5mRen6FYTAELb6jD/GY8BG1PPO7BPYDAlHRxub+/vpW5Ggy3EpCQykS8hziH+oxDXNShComGQ1FajdsCSahZQggvlZB5mJCz3iCSZKcTYAoL+DCgE5k0DBWYH2OohqXifgOC95n8PUayrhuaNuwlOFt+N7f36/iZCjc9ADD4fAduifuWRSh0+Pv/fOvXw7QcQy+KvAO3RSSTYimCMibkWFGFWIKFGgk0pTFjGTZI1JlUYBKCCvhSaOt+UBA0PHlOdQ7R0NJyR0iGiQfoS5ziu6B2cFQdE+kiSd2eNteoGxVX3yj3LGuz83LyBfftJAnj336ALlAT0XJdeOvycGqESY0hb6XBC1Yt5V4xh5e5sdSkkdVUUojijPKR7Yr1uKWvgzm1lHXLSR6A++h1dXbubQ20nUtzJ3w8/ZXry2+wPwG3Z1T7rnQr/Q30vPWt7wwRMScF8x5YTUYJ9jCc+qzJHRdKKaM+of9ZL9w86C3GFylr+1EUGSBm0IaFDbr7gvYOucbcDW8TNF24yV4VSAOTR4CN/NMwWb9ErauqgZsN3y03VgEf25vPb/MaiawKpZEx+NvXny9qa7ZDuqT6suYGw2A5YpSUzMHTlELqoxmGSuUYMna1LBZI/zDCqHmzoI9ZmiqV/ODmwaBNRQDXpip7gKo7rJCBAD1tRuQGElSjFkMuMrtU28XG3Y/rXX7+Omx8xrOai/x9cervW3cGNcXvvJZc8b1mOZrbU9pUVR3GttlgP9lmon72S6zs7W1sbWzF+kxVEp4q0IYVUIxvIUQh4o90dBQZSipKsyOFqELbQikW4vVUQ7hn7KN1Knd29uZWjz58PWHtbc1fsH0VY9/wNUR2tiDiq8+wOYwE4DZGpn/GLQ6ZOS1QJIkECLlaW8CsumPk+7UCDa7sN0Kj0HVY97ssO4E29cHTdyMbzAMPUxvry/BCiCsXNki/WJK7XvRFx/YA00+SPB19uKG0A4S6DhXxZjKmSI6aT0L2ueD3MksdiEO69qoa9fkrsyMELQPXvP9aVURWJ5dwb1P7woS4rLenVPTKqDWdHjNEkNELwt8molyRQyG7IfQekXqjFHQyqF1C8lGMONnyPECRaQoKJHKpIMZd6aRMll7zi8dFuZYkxGN2Zc2RCZ/SmUm18Zf7/XCdDnbDo7Q/vp6q91Ub5aYcZiCPvU/X8ABnQsREzsMmQnljot7jjuVV89gLti6XEObhzvXcPQB6sB0W92vS2fxno5MM7OmdWFtkyzY7oJg53AooyOAzKi6Mm1nmS7Xl/5M2RlZbpdtU3+myhbKMl2uaf2ZstzMxQoVtMpep3eayB4S+dAo6vip4mCt5PGRaObUmvSq04R68RwQdoTiRCumWiI98cUnr8s/AXE5LnMQR2tLAGFoy9L25wMUhp1lwq+lo1GC/2ZOWoV/MzGtwr+ZnVbhXIr2UDM2PRtWW1lbOVT6MaNHtf5/WG5eWVApswDjSNO8yOA1R0XDEl4DNY5V0ysOIx96mLAJYsm7BX81Q3FGlIInKbxzmnwfrBwdRiA/B80EMRT4FZppRh6N2Hjj6MJtYowPI1guRlacNaP5fw
+// Note that this cannot run in 2D mode to get zoom level since fov, fovy is undefined
+// instead use pixelSize to infer zoom level 
+// map scale vs zoom: https://docs.geoserver.org/latest/en/user/styling/ysld/reference/scalezoom.html
+// a google map 2D height vs zoom: https://stackoverflow.com/questions/36544209/converting-altitude-to-z-level-and-vice-versa
+// but this google map zoom seems a level + 1~2 with the following table
+// map scale/pixelSize //=> zoom level
+// 559082264/53132(+)  //=> 0 (ratio 10522.51)
+// 279541132/47478     //=> 1 (5887.803 upper limit of zoom 1)
+// 139770566/29029     //=> 2 (4814.86)
+// 69885283/18927      //=> 3 (3692.359)
+// 34942641/13673      //=> 4 (2555.594)
+// 17471321/11191      //=> 5 (1561.194)
+// 8735660/10175       //=> 6 (858.5415)
+// 4367830/9600        //=> 7 (454.9823)
+// 2183915/9321        //=> 8 (234.3005)
+
 var viewer = new Cesium.Viewer("cesiumContainer");
 var west, south, east, north;
 var toolbar = document.getElementById('toolbar');
 var camera = viewer.camera;
 
-//https://gist.github.com/ezze/d57e857a287677c9b43b5a6a43243b14
+//modified from https://gist.github.com/ezze/d57e857a287677c9b43b5a6a43243b14
 function detectZoomLevel(distance) {
     let scene = viewer.scene;
     let tileProvider = scene.globe._surface.tileProvider;
     let quadtree = tileProvider._quadtree;
     let drawingBufferHeight = viewer.canvas.height;
     let sseDenominator = viewer.camera.frustum.sseDenominator;
-
+ // let denom = 2.0 * Math.tan(0.5 * camera.frustum._fovy);
+  
+//  console.log(sseDenominator);
+//  console.log("denom: " + camera.frustum._fovy);
     for (let level = 0; level <= 19; level++) {
         let maxGeometricError = tileProvider.getLevelMaximumGeometricError(level);
-        let error = (maxGeometricError * drawingBufferHeight) / (distance * sseDenominator);
+      
+//      console.log("maxGeometricError: " + maxGeometricError);
+//      console.log("distance: " + distance);
+        let error = (maxGeometricError * drawingBufferHeight) / (distance * denom);
+//      console.log("error: " + error);
+//      console.log("quadtree.maximumScreenSpaceError: " + quadtree.maximumScreenSpaceError);
         if (error < quadtree.maximumScreenSpaceError) {
             return level;
         }
@@ -24,8 +48,9 @@ function detectZoomLevel(distance) {
     return null;
 }
 
-//https://gis.stackexchange.com/questions/129903/cesium-3d-determining-the-map-scale-of-the-viewed-globe
-viewer.clock.onTick.addEventListener(function () {
+//modified from https://gis.stackexchange.com/questions/129903/cesium-3d-determining-the-map-scale-of-the-viewed-globe
+viewer.camera.moveEnd.addEventListener(function () {
+//viewer.clock.onTick.addEventListener(function () {
     west = south = 999;
     east = north = -999;
 
@@ -54,21 +79,20 @@ viewer.clock.onTick.addEventListener(function () {
     var rect = viewer.camera.computeViewRectangle(viewer.scene.globe.ellipsoid,
         scratchRectangle);
     var pos = viewer.camera.position;
-    var cartographic = Cesium.Ellipsoid.WGS84.cartesianToCartographic(pos);
+    //var cartesian = viewer.scene.pickPosition(pos);
+    var cartographic = Cesium.Cartographic.fromCartesian(pos);
+    //var cartographic = Cesium.Ellipsoid.WGS84.cartesianToCartographic(pos);
     var height = cartographic.height;
+    //https://stackoverflow.com/questions/15331358/three-js-get-object-size-with-respect-to-camera-and-object-position-on-screen/15331885
+    var vFOV = camera.frustum.fov * Math.PI / 180;        // convert vertical fov to radians
+    var vheight = 2 * Math.tan( vFOV / 2 ) * height;
     var level = detectZoomLevel(height);
-    var bndSphere = //Cesium.BoundingSphere.fromPoints(pos);
-                    new Cesium.BoundingSphere();
-    var pixelSize = camera.getPixelSize(bndSphere, viewer.scene.drawingBufferWidth, viewer.scene.drawingBufferHeight);
+    //var modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(pos);
+    var boundingSphere = new Cesium.BoundingSphere();
+    //var newBoundingSphere = Cesium.BoundingSphere.transformWithoutScale(boundingSphere, modelMatrix);
+    var pixelSize = camera.getPixelSize(boundingSphere, viewer.scene.drawingBufferWidth, viewer.scene.drawingBufferHeight);
     //compute number of pixels that original ellipse appears to be
-    var sizeInPixels = (2 * bndSphere.radius) / pixelSize;
-/*  var newPoint = t._viewer.entities.add({
-      point: {
-        pixelSize: sizeInPixels,
-        color: Cesium.Color.GREEN,
-        heightReference: Cesium.HeightReference.CLAMP_TO_GROUND
-      }
-    }); */
+    //var sizeInPixels = (2 * boundingSPhere.radius) / pixelSize;
   
     if (west > 900) {
         toolbar.innerHTML = 'Location not known.';
@@ -79,8 +103,10 @@ viewer.clock.onTick.addEventListener(function () {
             'East: ' + Cesium.Math.toDegrees(east).toFixed(4) + '<br/>' +
             'North: ' + Cesium.Math.toDegrees(north).toFixed(4) + '<br/>' +
             'meters per pixel: ' + pixelSize + '<br/>' + 
+          //'sizeInPixels: ' + sizeInPixels + '<br/>' +  
             'position: ' + pos + '<br/>' + 
-            'height: ' + cartographic.height + '<br/>' + 
+            'height: ' + height + '<br/>' + 
+            'vheight: ' + vheight + '<br/>' + 
             'zoom level: ' + level + '<br/>' + 
             '-- view rect: --' + '<br/>' + 
             'West: ' + Cesium.Math.toDegrees(rect.west).toFixed(4) + '<br/>' +
