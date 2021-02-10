15e8b29c9d37b1d1a1d3a7da3bbc3bcac51869c5
Weng Chiyu <cyrain@gmail.com>
2021-02-10 09:45:46 +0800

    some problem fixed in WSL2 (ubuntu 20.04 on Win10

--- script/unix_useful_commands.sh
+++ script/unix_useful_commands.sh
@@ -13,4 +13,12 @@ sudo update-alternatives --config jar
 # bundle analyze
 webpack-bundle-analyzer --port 4200 build/stats.json
 
-# continue...
+# 
+# WSL2 for win10 (ubuntu20.04)
+# VxServ and Xfce4 (startxfce4) trouble shooting in WSL2: https://github.com/cascadium/wsl-windows-toolbar-launcher/blob/master/README.md#troubleshooting
+# After Xlaunch, got black screen: euid !=0,directory /tmp/.ICE-unix will not be created
+# https://lists.freebsd.org/pipermail/freebsd-questions/2005-May/087042.html
+sudo mkdir -m 1777 /tmp/.X11-unix
+sudo mkdir -m 1777 /tmp/.ICE-unix
+sudo startxfce4
+
