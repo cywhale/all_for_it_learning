ce403b056ab9960ea0b1c9a924f65b8aedb25b7d
Weng Chiyu <cyrain@gmail.com>
2021-04-11 09:01:12 +0800

    debug iptables prerouting...

--- script/unix_useful_commands.sh
+++ script/unix_useful_commands.sh
@@ -18,8 +18,33 @@ sudo /etc/init.d/networking restart
 #sudo nano /etc/resolvconf/resolv.conf.d/tail
 #sudo nano /etc/resolvconf/resolv.conf.d/base
 #sudo systemctl status resolvconf.service
+# See now which DNS resolved
+nslookup www.google.com
 
-# -----
+# remove iptables prerouting by opencpu (note: https://github.com/biigle/core/discussions/339
+# debug why input/output port not works, and see which port tcp i/o
+netstat -tulpn
+ngrep tcp and port 80
+# debug why 80 port no connection
+curl -I localhost
+wget https://your_host
+# debug port 80, 443 usage
+sudo netstat -tulpn | grep 80
+sudo netstat -tulpn | grep 443
+sudo nmap -sS 127.0.0.1 -p 80 
+
+# Find iptables 80,433 prerouting rules and remove them (after remove opencpu...)
+sudo iptables -t nat -nvL #just list
+sudo iptables -L OUTPUT  -n --line-numbers #according 'Chain' === 'OUTPUT' and listed the rules (under, & limited to this Chain) by line numbers
+# Note: -D is delete, and according the list line-number of the rule you want to deleted (assume N, N=1,2,3,...)
+sudo iptables -t nat -D OUTPUT N
+
+# if modify /etc/hosts /etc/hostname
+sudo systemctl restart systemd-hostnamed
+sudo /etc/init.d/networking restart
+curl -I localhost
+
+# ---- others ----
 # Node devs
 # bundle analyze
 webpack-bundle-analyzer --port 4200 build/stats.json
