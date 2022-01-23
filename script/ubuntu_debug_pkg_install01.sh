2f93733277a7d68690492233c4b19274d6806abe
Weng Chiyu <cyrain@gmail.com>
2022-01-23 21:38:16 +0800

    libgeos for undefined symbol: GEOSMakeValidWithParams_r

--- script/ubuntu_debug_pkg_install01.sh
+++ script/ubuntu_debug_pkg_install01.sh
@@ -14,6 +14,31 @@ sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable
 sudo apt-get update
 sudo apt-get install --reinstall libudunits2-dev libgdal-dev libgeos-dev libproj-dev proj-bin gdal-bin
 
+# and also for getting duplicated gdal
+# double free or corruption (out) ==> Aborted (core dumped) # https://github.com/r-spatial/sf/issues/1393
+# Please refer this written by Edzer Pebesma: # https://github.com/r-spatial/sf#multiple-gdal-geos-andor-proj-versions-on-your-system
+# Multiple GDAL, GEOS and/or PROJ versions on your system
+# If you use dynamic linking (installation from source) and have multiple versions of these libraries installed 
+# (e.g. one from ubuntugis-unstable, another installed from source in /usr/local/lib) then 
+# this will in general not work, even when setting LD_LIBRARY_PATH manually. See here for the reason why.
+
+# Error: ogr2ogr: symbol lookup error: /usr/lib/libgdal.so.30: undefined symbol: GEOSMakeValidWithParams_r
+# refer (but not reall work, but you can check) https://github.com/OSGeo/gdal/issues/2214 
+# apt-cache policy libgdal-dev
+# apt-cache policy libproj-dev
+# apt-cache policy libgeos-dev
+# solution: cmake & make install libgeos (capi) as the same version with libgeos-dev #https://libgeos.org/usage/download/
+
+# First, completely remove gdal from Ubuntu 20.04 # https://lists.osgeo.org/pipermail/gdal-dev/2020-August/052465.html
+sudo apt purge $(dpkg -l | grep gdal | awk '{print $2}' | xargs)
+# remove  all installed versions of gdal, geos and proj # https://github.com/r-spatial/sf/issues/1685
+sudo apt-get remove libproj-dev:amd64 
+sudo apt-get remove libproj19:amd64 
+sudo apt-get remove libgeos-dev
+sudo apt-get remove libgeos-3.9.0:amd64 
+sudo apt-get remove libgdal-dev
+
+
 # dependency conflicts due to package upgrade and still some other dependency no such corresponding upgrade
 # The following packages have unmet dependencies:  ffmpeg : Depends: libavcodec58....no going to be installed problem
 # apt-get install -f not work
