1241ef4a8298f96e039bc032050df0b56732b088
Weng Chiyu <cyrain@gmail.com>
2022-05-30 20:44:59 +0800

    change tempdir not really work in opencpu debugging but add TZ in Renviron work

--- script/opencpu-server_debug.sh
+++ script/opencpu-server_debug.sh
@@ -15,13 +15,21 @@ sudo nano /etc/apparmor.d/opencpu.d/custom
 # documentation: https://manpages.ubuntu.com/manpages/xenial/man5/apparmor.d.5.html
 sudo service apparmor reload
 
-# weird since Ubuntu20.04/opencpu2.2 saved temp file in /tmp/systemd-private-xxxxxxx/tmp/ocpu-store/...
-# tmp files location
-sudo nano /etc/opencpu/server.conf.d/tempdir.conf
+# weird since Ubuntu20.04/opencpu2.2 saved temp file in /tmp/systemd-private-xxxxxxx-apache2.service-xxx/tmp/ocpu-store/...
+# BUT this private dirctory cannot be seen from others (but can mv to /tmp/ocpu-temp and chown -R www-data:www-data /tmp/ocpu-temp && chmod -R g+rwx /tmp/ocpu-temp)
+# Then tmp files location can be changed. But Not do it. I cannot find a good way to specify temp dir...
+# sudo nano /etc/opencpu/server.conf.d/tempdir.conf
 #{
 #  "tempdir" : "/tmp/ocpu-temp" 
 #}
-## <- it created a /tmp/ocpu-temp/tmp/ocpu-store/... 
+## <- it created a /tmp/ocpu-temp/tmp/ocpu-temp/ocpu-store/... (really a mess...)
+## Delete /tmp/systemd-private-xxxxxxx-apache2.service-xxx will cause apachectl fail. Stop/disable apache2 and re-enable/start it.
+
+#ERROR in session /ocpu/tmp/x0xxxxxxx (under systempd-private-...)
+## Message: <simpleWarning in system("timedatectl", intern = TRUE): running command 'timedatectl' had status 1>
+timedatectl #see your timezone
+# sudo nano /etc/opencpu/Renviron
+# TZ="Asia/Taipei"
 
 #RESTART
 sudo apachectl restart
