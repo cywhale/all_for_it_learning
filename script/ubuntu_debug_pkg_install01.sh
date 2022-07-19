74bbc49edbb7771740bede7634e246f751ff1eb4
Weng Chiyu <cyrain@gmail.com>
2022-07-19 15:20:05 +0800

    install cuda for ubuntu20.04/n1

--- script/ubuntu_debug_pkg_install01.sh
+++ script/ubuntu_debug_pkg_install01.sh
@@ -3,6 +3,15 @@
 nano /home/odbadmin/.cache/node-gyp/16.11.1/include/node/v8-internal.h
 #and replace remove_cv_t to remove_cv
 
+#CUDA
+wget https://developer.download.nvidia.com/compute/cuda/11.6.2/local_installers/cuda-repo-ubuntu2004-11-6-local_11.6.2-510.47.03-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2004-11-6-local_11.6.2-510.47.03-1_amd64.deb 
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-60
+sudo apt update
+
+
 #Python
 #vscode jupyter: The kernel failed to start due to the missing module 'prompt_toolkit.formatted_text'. Consider installing this module. View Jupyter [log](command:jupyter.viewOutput) for further details.
 #solution: reinstall ipykernel
