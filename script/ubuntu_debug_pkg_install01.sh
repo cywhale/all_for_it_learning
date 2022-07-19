65ae66d8378ff2ade417389f9a147f610d72d82b
Weng Chiyu <cyrain@gmail.com>
2022-07-19 16:11:43 +0800

    install cuda(not yet)/n2

--- script/ubuntu_debug_pkg_install01.sh
+++ script/ubuntu_debug_pkg_install01.sh
@@ -8,9 +8,9 @@ wget https://developer.download.nvidia.com/compute/cuda/11.6.2/local_installers/
 sudo dpkg -i cuda-repo-ubuntu2004-11-6-local_11.6.2-510.47.03-1_amd64.deb 
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-60
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt update
-
+sudo apt install cuda
 
 #Python
 #vscode jupyter: The kernel failed to start due to the missing module 'prompt_toolkit.formatted_text'. Consider installing this module. View Jupyter [log](command:jupyter.viewOutput) for further details.
