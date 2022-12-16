df6d439f044713fb996da7fd9cb2024dfa1d642a
Weng Chiyu <cyrain@gmail.com>
2022-12-16 15:22:50 +0800

    ST_CreateGrid2 without lon, lat seems change the order of rows? cause gid diff with ST_CreateGrid()

--- script/sql/postgis_025gridded.sql
+++ script/sql/postgis_025gridded.sql
@@ -58,27 +58,8 @@ SELECT dblink_disconnect('conn1');
 
 create extension POSTGIS;
 
-/* create a smaller (without lon/lat) 0.25-gridded Fishnet for marineheatwave */
-CREATE OR REPLACE FUNCTION ST_CreateGrid2(
-        nrow integer, ncol integer,
-        xsize float8, ysize float8,
-        x0 float8 DEFAULT 0, y0 float8 DEFAULT 0,
-        OUT "row" integer, OUT col integer,
-        OUT longitude float8, OUT latitude float8,
-        OUT geom geometry)
-    RETURNS SETOF record AS
-$$
-SELECT i + 1 AS row, j + 1 AS col, 
-       $5 + $3 * 0.5 + j * $3 AS longitude, $6 + $4 * 0.5 + i * $4 AS latitude,  
-       ST_Translate(cell, j * $3 + $5, i * $4 + $6) AS geom
-FROM generate_series(0, $1 - 1) AS i,
-     generate_series(0, $2 - 1) AS j,
-(
-SELECT ('POLYGON((0 0, 0 '||$4||', '||$3||' '||$4||', '||$3||' 0,0 0))')::geometry AS cell
-) AS foo;
-$$ LANGUAGE sql IMMUTABLE STRICT;
-
-CREATE TABLE grid_025gr AS SELECT * FROM ST_CreateGrid2(720,1440,0.25,0.25,-180,-90);
+/* create 0.25-gridded Fishnet for marineheatwave */
+CREATE TABLE grid_025gr AS SELECT * FROM ST_CreateGrid(720,1440,0.25,0.25,-180,-90);
 UPDATE grid_025gr SET geom = ST_SetSRID (geom, 4326);
 CREATE INDEX grid_025gr_geom ON grid_025gr USING gist (geom);
 ALTER TABLE grid_025gr ADD gid serial PRIMARY KEY;
