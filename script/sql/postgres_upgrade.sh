b5adeb0fd102b331b0ca7c1d11d44cd125f21ca5
Weng Chiyu <cyrain@gmail.com>
2023-03-30 15:00:37 +0800

    pg_upgrade for Posgresql with timescaledb

--- script/sql/postgres_upgrade.sh
+++ script/sql/postgres_upgrade.sh
@@ -1,3 +1,30 @@
+#==== Note old postgresql upgrade with timescaledb tricks not work anymore, just use pg_upgrade ====
+# BUT cannot use mixed version timescaledb (i.e cannot upgrade old-postgres-with-older-timescaledb to new-postgres-with-newer-timescaledb
+# refer: https://github.com/timescale/timescaledb/issues/2009
+# refer: https://severalnines.com/blog/upgrading-postgresql-11-postgresql-13-timescaledb-and-postgis-linux-using-pgupgrade/
+# refer: https://github.com/timescale/timescaledb/issues/1425
+# check your current timescaledb version with $psql -> (in postgres) \dx
+# check compatable timescaledb with postgrsql https://docs.timescale.com/self-hosted/latest/upgrades/upgrade-pg/
+# install newer timescaledb on linux follow: https://docs.timescale.com/self-hosted/latest/install/installation-linux/
+# for example if you install timescaledb-2-postgresql-15 for Postgresql 15, and you want to upgrade Postgresql 12 to 15
+# then you need to install timescaledb-2-postgresql-12 for Postgresql 12 too and update all your databases which have timescaledb extension
+# for example, if your postgres, tsdb databases have timescaledb extension
+sudo -u postgres psql -d postgres -qxc "ALTER EXTENSION timescaledb UPDATE;" #can also run in psql terminal
+sudo -u postgres psql -d tsdb -qxc "ALTER EXTENSION timescaledb UPDATE;"
+# Then check pg_upgrade --check to see if any other errors!!!
+# Note!!! add -O "-c timescaledb.restoring='on'" 
+sudo -iu postgres /usr/lib/postgresql/15/bin/pg_upgrade -o "-c config_file=/etc/postgresql/12/main/postgresql.conf" --old-datadir=/var/lib/postgresql/12/main/   -O "-c config_file=/etc/postgresql/15/main/postgresql.conf"  --new-datadir=/var/lib/postgresql/15/main/ --old-bindir=/usr/lib/postgresql/12/bin --new-bindir=/usr/lib/postgresql/15/bin -O "-c timescaledb.restoring='on'" --check
+
+# If no other errors, turn off --check
+sudo -iu postgres /usr/lib/postgresql/15/bin/pg_upgrade -o "-c config_file=/etc/postgresql/12/main/postgresql.conf" --old-datadir=/var/lib/postgresql/12/main/   -O "-c config_file=/etc/postgresql/15/main/postgresql.conf"  --new-datadir=/var/lib/postgresql/15/main/ --old-bindir=/usr/lib/postgresql/12/bin --new-bindir=/usr/lib/postgresql/15/bin -O "-c timescaledb.restoring='on'"
+## When upgrade successful, it gives:
+#Once you start the new server, consider running:
+#    /usr/lib/postgresql/15/bin/vacuumdb --all --analyze-in-stages
+
+#Running this script will delete the old cluster's data files:
+#    ./delete_old_cluster.sh
+
+#=========================================
 dpkg --get-selections | grep postgres
 sudo apt install posgresql-14
 psql -U xxx -d xxx
