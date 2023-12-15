a4a16b1f31636237fa0b074e052863dc1d3bb76a
Weng Chiyu <cyrain@gmail.com>
2023-12-15 13:46:05 +0800

    Convert SRTM15_plus NetCDF to tif, then to quantized-mesh tiles

--- cesium/create_bathymetry_terrain.sh
+++ cesium/create_bathymetry_terrain.sh
@@ -1,7 +1,7 @@
 # from cesium_terrain_builder tumgis/ctb-dquantized-mesh
 # https://github.com/tum-gis/cesium-terrain-builder-docker
 docker pull tumgis/ctb-dquantized-mesh
-# put your terrain gtiff under your data directory
+# put your terrain gtiff under your data directory or see the example to convert .nc to tif at the end
 # or
 #docker cp /YourData/geo.tif ctb:/data/
 #docker cp /YourData/geo.vrt ctb:/data/
@@ -25,7 +25,7 @@ cd ~/go/src/github.com/geo-data/cesium-terrain-server/cmd/cesium-terrain-server/
 # vi main.go
 # needs
 ```
-//  DhinendraRajapakse's solution:
+//  DhinendraRajapakse solution:
 
 https := flag.Bool("https", false, "if https is enabled")
 pemPath := flag.String("pemPath", "", "Path to pem files cert.pem and key.pem")
@@ -114,3 +114,23 @@ cesium-terrain-server -dir /data/tilesets/terrain -port 8038 -pemPath /YourPem -
 #        requestWaterMask: false,
 #        requestVertexNormals:  true,
 #    });
+#
+# Convert SRTM15_plus NetCDF to tif, then to quantized-mesh tiles. The tiff is too big, so we cut off some regions for applications
+# For example, to convert the region from lower-left S: -60, E: 150, to upper-right N: -20, W: -160 (Zealandia)
+# Note that the region crossing the 180° line, so need two-steps gdal_translate
+
+gdal_translate -of GTiff -projwin 150 -20 180 -60 -a_srs EPSG:4326 -a_nodata -99999 -co BLOCKXSIZE=65 -co BLOCKYSIZE=65 -co BIGTIFF=YES NETCDF:"SRTM15_V2.5.5.nc":z zealandia_eastern.tif
+gdal_translate -of GTiff -projwin -180 -20 -160 -60 -a_srs EPSG:4326 -a_nodata -99999 -co BLOCKXSIZE=65 -co BLOCKYSIZE=65 -co BIGTIFF=YES NETCDF:"SRTM15_V2.5.5.nc":z zealandia_western.tif
+
+gdalwarp -co BIGTIFF=YES -co COMPRESS=DEFLATE zealandia_eastern.tif zealandia_western.tif srtm15_plus/srtm15_wgs84_n-20_s-60_e150_w-160.tif -t_srs "+proj=longlat +ellps=WGS84"
+gdaladdo -r average srtm15_plus/srtm15_wgs84_n-20_s-60_e150_w-160.tif 2 4 8 16 32
+
+gdalbuildvrt ./srtm15_plus/srtm15_wgs84_n-20_s-60_e150_w-160.vrt ./srtm15_plus/srtm15_wgs84_n-20_s-60_e150_w-160.tif
+
+export GDAL_CACHEMAX=8192 && docker run --rm -it --name ctb -v "/YourData/srtm15_v2_5_5/srtm15_plus/":"/data" tumgis/ctb-quantized-mesh
+
+# in docker
+mkdir -p terrain_n-20_s-60_e150_w-160
+ctb-tile -f Mesh -c 6 -C -N -o terrain_n-20_s-60_e150_w-160 -s 14 -e 0 srtm15_wgs84_n-20_s-60_e150_w-160.vrt
+ctb-tile -f Mesh -c 6 -C -N -l -o terrain_n-20_s-60_e150_w-160 -s 14 -e 0 srtm15_wgs84_n-20_s-60_e150_w-160.vrt
+
